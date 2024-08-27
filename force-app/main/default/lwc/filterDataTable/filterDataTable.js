f794b83966940330208daeecf9bb56689ca03ec7
sotoaugusto <sotoaugustoa@gmail.com>
2024-08-26 21:09:46 -0600

    add imperative apex calling instead of wire, wip save inline editing

--- force-app/main/default/lwc/filterDataTable/filterDataTable.js
+++ force-app/main/default/lwc/filterDataTable/filterDataTable.js
@@ -2,10 +2,11 @@
  * Created by ausoto on 2024-08-19.
  */
 
-import { LightningElement, wire } from 'lwc';
+import {api, LightningElement, wire} from 'lwc';
 //import apex shortcut
 //get records from soql
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import updateAccount from '@salesforce/apex/AccountController.updateAccount';
 //import lightning shortcut
 //for actions
 import { NavigationMixin } from 'lightning/navigation';
@@ -13,32 +14,35 @@ import { NavigationMixin } from 'lightning/navigation';
 import { refreshApex } from "@salesforce/apex";
 //update ui
 import { updateRecord } from "lightning/uiRecordApi";
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
 
 //array of objects with two properties
 // dropdown actions
-const actions = [
+const ACTIONS = [
         {label: 'View', name: 'view'},
         {label: 'Edit', name: 'edit'},
     ];
 
-const columns = [
+const COLUMNS = [
         {label: 'Name', fieldName:'Name', editable:true},
         {label: 'Industry', fieldName:'Industry', editable:true},
         {label: 'AnnualRevenue', fieldName:'AnnualRevenue', editable:true},
         {
             type:'action',
-            typeAttributes: {rowActions: actions},
+            typeAttributes: {rowActions: ACTIONS},
         },
     ]
 
 
 export default class FilterDataTable extends NavigationMixin(LightningElement) {
-  availableAccounts;
+  @api availableAccounts;
   initialRecords;
-  columns = columns;
+  columns = COLUMNS;
   //handle inline editing TODO
   draftValues = [];
   error;
+
+
   // whereClauses = {
   // // key, value
   //   BillingCity: 'Lawrence',
@@ -126,10 +130,37 @@ export default class FilterDataTable extends NavigationMixin(LightningElement) {
 
 
 
+  getAccountData(whereClauses){
+    getAccounts({whereClauses: this.whereClauses})
+        .then(data =>{
+          if (data) {
+            console.log('🚀 data from imperative getAccounts: ', data);
+            this.availableAccounts = data;
+            this.initialRecords = data;
+
+          }
+    }).catch(error =>{
+      console.log(error);
+      })
+  }// end getAccountData
+
+
+
+
+  // runs on page load, similar to @wire
+  connectedCallback() {
+    this.getAccountData(this.whereClauses);
+  }
+
+
+// @wire(updateAccount,{recordId: '$recordId'})
+// accountsUpdated;
 
 // wire runs on page load
 // get all account records from controller, then save them to availableAccounts and initialRecords for display
-@wire(getAccounts, {whereClauses: '$whereClauses' })
+
+  // @wire(getAccounts, {whereClauses: '$whereClauses' })
+
 //propertyOrFunction—A private property or function that receives the stream of data from the wire service.
 //If a property is decorated with @wire, the results are returned to the property’s data property or error property.
 //If a function is decorated with @wire, the results are returned in an object with a data property or an error property.
@@ -137,47 +168,49 @@ export default class FilterDataTable extends NavigationMixin(LightningElement) {
 //Note
 //The data property and the error property are hardcoded values in the API. You must use these values.
 // in this case, im using a function wiredAccount
-wiredAccount({error,data}){
-  if (data) {
-    console.log('🚀 data from @wire getAccounts: ', data);
-    this.availableAccounts = data;
-    this.initialRecords = data;
 
-  } else if(error){
-    this.error = error;
-    this.availableAccounts = undefined;
-  }
-}
+//   wiredAccount({error,data}){
+//   if (data) {
+//     console.log('🚀 data from @wire getAccounts: ', data);
+//     this.availableAccounts = data;
+//     this.initialRecords = data;
+//
+//   } else if(error){
+//     this.error = error;
+//     this.availableAccounts = undefined;
+//   }
+// }
 
-handleRowAction (event){
-  const actionName = event.detail.action.name;
-  const row = event.detail.row;
-  row.Id = undefined;
 
-  switch (actionName){
-    case 'view':
-      this[NavigationMixin.Navigate]({
-        type: 'standard__recordPage',
-        attributes:{
-          recordId:row.Id,
-          objectApiName: 'Account',
-          actionName: 'view'
-        }
-      });
-      break;
-    case 'edit':
-      this[NavigationMixin.Navigate]({
-        type: 'standard__recordPage',
-        attributes:{
-          recordId:row.Id,
-          objectApiName: 'Account',
-          actionName: 'edit'
-        }
-      });
-      break;
-    default:
-  }//end switch
-}//end handleRowAction
+  handleRowAction (event){
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    row.Id = undefined;
+
+    switch (actionName){
+      case 'view':
+        this[NavigationMixin.Navigate]({
+          type: 'standard__recordPage',
+          attributes:{
+            recordId:row.Id,
+            objectApiName: 'Account',
+            actionName: 'view'
+          }
+        });
+        break;
+      case 'edit':
+        this[NavigationMixin.Navigate]({
+          type: 'standard__recordPage',
+          attributes:{
+            recordId:row.Id,
+            objectApiName: 'Account',
+            actionName: 'edit'
+          }
+        });
+        break;
+      default:
+    }//end switch
+  }//end handleRowAction
 
 handleSearch(event) {
   const searchKey = event.target.value.toLowerCase();
@@ -222,39 +255,78 @@ handleAccountInputFields(event) {
     // console.log(this.whereClauses);
   }
 }
+  // https://developer.salesforce.com/docs/platform/lwc/guide/apex-call-imperative.html
+   handleSubmit() {
+    // try {
+    //   // send whereClauses map, returns SOQL with the new criteria filtered
+    //   this.availableAccounts = await getAccounts({ whereClauses: this.whereClauses });
+    //   this.error = undefined;
+    //   console.log('🚀 data from imperative getAccounts with submit button: ', this.availableAccounts);
+    //   // TODO how can i console log my map?
+    //   // console.log(JSON.stringify([this.whereClauses.entries()]));
+    //   // console.log([...this.whereClauses.entries()]);
+    //   // console.log([...Object.entries(this.whereClauses)]);
+    //
+    // } catch (error) {
+    //   this.error = error;
+    //   this.availableAccounts = undefined;
+    //
+    //   console.log(this.error);
+    //
+    // }
+  try{
+    this.getAccountData(this.whereClauses);
 
-  async handleSubmit() {
-    try {
-      // send whereClauses map, returns SOQL with the new criteria filtered
-      this.availableAccounts = await getAccounts({ whereClauses: this.whereClauses });
-      this.error = undefined;
-      console.log('🚀 data from imperative getAccounts with submit button: ', this.availableAccounts);
-      // TODO how can i console log my map?
-      // console.log(JSON.stringify([this.whereClauses.entries()]));
-      // console.log([...this.whereClauses.entries()]);
-      // console.log([...Object.entries(this.whereClauses)]);
+  }catch (error) {
+    console.error('error:',error);
+  }
 
-    } catch (error) {
-      this.error = error;
-      this.availableAccounts = undefined;
-
-      console.log(this.error);
-
-    }
   }//end handleSubmit
 
-//   getAccounts({whereClauses: this.whereClauses })
-//     .then(result => {
-//       if (result) {
-//           this.results = result;
-//           console.log('Query Results:', this.results);
-//       }
-// //    todo once it's working, save results to availableAccounts
-//   })
-//     .catch(error => {
-//     console.error('Error:', error);
-//   });
+  async handleSave(event) {
+    // Convert datatable draft values into record objects
+    const records = event.detail.draftValues.slice().map((draftValue) => {
+      const fields = Object.assign({}, draftValue);
+      return {fields};
+    });
 
+    // Clear all datatable draft values
+    this.draftValues = [];
+
+    try {
+      // Update all records in parallel thanks to the UI API
+      const recordUpdatePromises = records.map((record) => updateRecord(record));
+      await Promise.all(recordUpdatePromises);
+
+      // Report success with a toast
+      this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Account updated",
+            variant: "success"
+          })
+      );
+      //todo, implement imperative apex call instead of refreshApex (used with wire)
+                                                          console.log('accountsUpdated before',this.accountsUpdated);
+                                                           console.log('availableAccounts before',this.availableAccounts);
+
+
+      // Display fresh data in the datatable
+      // await refreshApex(this.availableAccounts);
+      // this.availableAccounts = accountsUpdated.data;
+                                                            console.log('accountsUpdated after',this.accountsUpdated);
+                                                           console.log('availableAccounts after',this.availableAccounts);
+
+    } catch (error) {
+      this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error updating or reloading contacts",
+            message: error.body.message,
+            variant: "error"
+          })
+      );
+    }
+  }//end handleSave
 
 
 }//end filterDataTable
