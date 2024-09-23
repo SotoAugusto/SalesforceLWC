48ece4a471609a19e79aa7abbe4fdcfebc1194d1
sotoaugusto <sotoaugustoa@gmail.com>
2024-09-22 23:32:00 -0600

    test: Configure Jest testing framework

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,37 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+// setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  moduleNameMapper: {
+    /* CSS library import fix in test context. See:
+    https://github.com/salesforce/sfdx-lwc-jest/issues/288) */
+    //any css file that is imported in the js file will be mocked with the following path
+    "^c/obd_SharedStyle$":
+      "<rootDir>/force-app/main/default/lwc/obd_SharedStyle/obd_SharedStyle.css",
+    "^c/orderingSharedStyle$":
+      "<rootDir>/force-app/main/default/lwc/orderingSharedStyle/orderingSharedStyle.css",
+    "^c/orderingSharedStyle$":
+      "<rootDir>/force-app/main/default/lwc/orderingMobileScreenStyles/orderingMobileScreenStyles.css",
+    // Jest mocks
+    "^@salesforce/apex$": "<rootDir>/force-app/test/jest-mocks/apex",
+    "^@salesforce/schema$": "<rootDir>/force-app/test/jest-mocks/schema",
+    "^lightning/navigation$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/navigation",
+    "^lightning/platformShowToastEvent$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent",
+    "^lightning/uiRecordApi$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/uiRecordApi",
+    "^lightning/messageService$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/messageService",
+    "^lightning/actions$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/actions",
+    "^lightning/modal*": "<rootDir>/force-app/test/jest-mocks/lightning/modal",
+    "^lightning/refresh$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/refresh",
+  },
+  setupFiles: ["jest-canvas-mock"],
+  setupFilesAfterEnv: [],
+  testTimeout: 10000,
 };
