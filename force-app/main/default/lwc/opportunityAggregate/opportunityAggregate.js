739a9c481e5bf9be1f42bb3c9a17bb5d318e3fbf
sotoaugusto <sotoaugustoa@gmail.com>
2024-10-07 09:57:37 -0600

    feat: add wrapper class to relate AccountOppStats with opportunitiesList

--- force-app/main/default/lwc/opportunityAggregate/opportunityAggregate.js
+++ force-app/main/default/lwc/opportunityAggregate/opportunityAggregate.js
@@ -1,46 +1,44 @@
 import { LightningElement, wire, track } from "lwc";
-import getOpportunitiesByAccount from "@salesforce/apex/OpportunityController.getOpportunitiesByAccount";
+import getAccountOppStats from "@salesforce/apex/OpportunityController.getAccountOppStats";
 
 export default class OpportunityAggregate extends LightningElement {
-  COLUMNS = [
-    { label: "Account Id", fieldName: "Id" },
-    { label: "Account Name", fieldName: "Name" },
-    {
-      label: "Annual Revenue Sum",
-      fieldName: "AnnualRevenueSum",
-      type: "currency",
-    },
-    {
-      label: "Total Opportunities",
-      fieldName: "TotalOpportunities",
-      type: "number",
-    },
-    {
-      label: "Total Opportunity Amount",
-      fieldName: "TotalOpportunityAmount",
-      type: "currency",
-    },
-    {
-      label: "Max Opportunity Amount",
-      fieldName: "MaxOpportunityAmount",
-      type: "currency",
-    },
-    {
-      label: "Avg Revenue Per Opportunity",
-      fieldName: "AvgRevenuePerOpportunity",
-      type: "currency",
-    },
-  ];
-  @track currentOpportunitiesList;
-  @wire(getOpportunitiesByAccount)
+  // COLUMNS = [
+  //   { label: "Account Id", fieldName: "Id" },
+  //   { label: "Account Name", fieldName: "Name" },
+  //   {
+  //     label: "Annual Revenue Sum",
+  //     fieldName: "AnnualRevenueSum",
+  //     type: "currency",
+  //   },
+  //   {
+  //     label: "Total Opportunities",
+  //     fieldName: "TotalOpportunities",
+  //     type: "number",
+  //   },
+  //   {
+  //     label: "Total Opportunity Amount",
+  //     fieldName: "TotalOpportunityAmount",
+  //     type: "currency",
+  //   },
+  //   {
+  //     label: "Max Opportunity Amount",
+  //     fieldName: "MaxOpportunityAmount",
+  //     type: "currency",
+  //   },
+  //   {
+  //     label: "Avg Revenue Per Opportunity",
+  //     fieldName: "AvgRevenuePerOpportunity",
+  //     type: "currency",
+  //   },
+  // ];
+  @track accountOpportunityData;
+  @wire(getAccountOppStats)
   opportunitiesList({ error, data }) {
     if (data) {
-      console.log("✅ data from @wire opportunitiesList: ", data);
-      this.currentOpportunitiesList = data;
-      // this.initialRecords = data;
+      console.log("✅ data from @wire accountOpportunityData: ", data);
+      this.accountOpportunityData = data;
     } else if (error) {
-      console.log("❌ error from @wire opportunitiesList: ", error);
-      // this.availableAccounts = undefined;
+      console.log("❌ error from @wire accountOpportunityData: ", error);
     }
   } //end wire
 } //end component
